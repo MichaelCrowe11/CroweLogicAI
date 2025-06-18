7585a1f04d26258ed30750cb834769ad6279aa7c
Michael Crowe <michael@southwestmushrooms.online>
2025-06-18 11:30:22 -0500

    Fix hydration mismatch error in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} bg-mycelium`}>
         <ThemeProvider attribute="class" defaultTheme="light">
           <SidebarProvider>
